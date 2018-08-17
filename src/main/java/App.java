612e2bf1f7c639165910e2010200b190ac6e051f
meimein <iantanxjw@gmail.com>
2018-08-17 13:38:48 +1000

    hibernate practice tutorial

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App
+{
+    public static void main (String [] args)
+    {
+        Customer bob = new Customer();
+        bob.setCid(101);
+        bob.setName("bob");
+
+        Configuration con = new Configuration().configure().addAnnotatedClass(Customer.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+
+        Transaction tx = session.beginTransaction();
+
+        session.save(bob);
+
+        tx.commit();
+    }
+}
---
+++ src/main/java/Customer.java
@@ -0,0 +1,21 @@
+public class Customer
+{
+    private int cid;
+    private String name;
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        cid = cid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
